6faca21d8a2ca118decaf1e12b40e93831bb41d6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-09-22 22:58:59 +0100

    simpler logic; basic template generation

--- plugin.groovy
+++ plugin.groovy
@@ -4,9 +4,14 @@ import com.intellij.psi.PsiFileSystemItem
 import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.PsiMethod
 
+import java.util.regex.Matcher
+
+import static HttpUtil.asJsString
+import static HttpUtil.createFromTemplate
 import static liveplugin.PluginUtil.*
 
 static List<PsiMethod> allMethodsIn(PsiJavaFile javaFile) {
+	// TODO exclude interfaces?
   def result = []
   javaFile.acceptChildren(new JavaRecursiveElementVisitor() {
     @Override void visitMethod(PsiMethod method) { result << method }
@@ -23,6 +28,7 @@ static List<PsiField> allFieldsIn(PsiJavaFile javaFile) {
 }
 
 static int amountOfParametersIn(PsiMethod method) {
+	// TODO exclude interfaces?
   method.parameterList.parametersCount
 }
 
@@ -30,12 +36,39 @@ static TreeMap makeHistogram(List values) {
   new TreeMap(values.groupBy{it}.collectEntries{[it.key, it.value.size()]})
 }
 
-class Histogram {
-	final TreeMap map = new TreeMap()
+class HttpUtil {
+	static void createFromTemplate(String templateFolder, String template, String projectName, String jsValue) {
+		def templateText = new File("$templateFolder/$template").readLines().join("\n")
+		def text = inlineJSLibraries(templateText) { fileName -> new File("$templateFolder/$fileName").readLines().join("\n") }
+		text = fillDataPlaceholder(text, jsValue)
+		text = fillProjectNamePlaceholder(text, "\"$projectName\"")
+		new File("${templateFolder}/${projectName}_${template}").write(text)
+	}
 
-	def add(int value) {
-		if (map[value] == null) map[value] = 0
-		map[value] = map[value] + 1
+	static String inlineJSLibraries(String html, Closure<String> sourceCodeReader) {
+		(html =~ /(?sm).*?<script src="(.*?)"><\/script>.*/).with{
+			if (!matches()) html
+			else inlineJSLibraries(
+					html.replace("<script src=\"${group(1)}\"></script>", "<script>${sourceCodeReader(group(1))}</script>"),
+					sourceCodeReader
+			)
+		}
+	}
+
+	static String fillProjectNamePlaceholder(String templateText, String projectName) {
+		templateText.replaceFirst(/(?s)\/\*project_name_placeholder\*\/.*\/\*project_name_placeholder\*\//, Matcher.quoteReplacement(projectName))
+	}
+
+	static String fillDataPlaceholder(String templateText, String jsValue) {
+		templateText.replaceFirst(/(?s)\/\*data_placeholder\*\/.*\/\*data_placeholder\*\//, Matcher.quoteReplacement(jsValue))
+	}
+
+	static String asJsString(List list) {
+		String newLine = "\\n\\\n"
+		def result = "\""
+		result += "metric" + newLine
+		result += list.join(newLine)
+		result + newLine + "\""
 	}
 }
 
@@ -43,9 +76,9 @@ registerAction("miscProjectHistograms", "ctrl shift H") { event ->
   def project = event.project
   doInBackground("Building histograms") {
     runReadAction {
-	    def amountOfMethodsInClass = new Histogram()
-	    def amountOfFieldsInClass = new Histogram()
-	    def amountOfParametersInMethod = new Histogram()
+	    def amountOfMethodsInClass = []
+	    def amountOfFieldsInClass = []
+	    def amountOfParametersInMethod = []
 
       for (PsiFileSystemItem item : allPsiItemsIn(project)) {
 	      if (item == null || !(item instanceof PsiJavaFile)) continue
@@ -60,10 +93,11 @@ registerAction("miscProjectHistograms", "ctrl shift H") { event ->
 	      }
       }
 
-      show(amountOfFieldsInClass.map)
-      show(amountOfMethodsInClass.map)
-      show(amountOfParametersInMethod.map)
+	    createFromTemplate("${pluginPath}/templates", "histogram.html", project.name, asJsString(amountOfParametersInMethod))
+      show(amountOfFieldsInClass.size())
+      show(amountOfMethodsInClass.size())
+      show(amountOfParametersInMethod.size())
     }
   }
 }
-
+show("reloaded")
