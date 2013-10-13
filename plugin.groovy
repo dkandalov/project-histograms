31a1916a3fd2b787cccfc427e6e0725af1d77c55
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-10-13 20:48:22 +0100

    extracted ProjectHistograms class

--- plugin.groovy
+++ plugin.groovy
@@ -9,32 +9,6 @@ import static liveplugin.PluginUtil.*
 import static templates.HtmlUtil.asJsArray
 import static templates.HtmlUtil.createFromTemplate
 
-static List<PsiMethod> allMethodsIn(PsiJavaFile javaFile) {
-  def result = []
-  javaFile.acceptChildren(new JavaRecursiveElementVisitor() {
-    @Override void visitMethod(PsiMethod method) {
-	    // ignore interfaces assuming that everything will be counted in their implementations
-	    // or if there are not implementations, then it probably can be ignored
-	    if (method.containingClass.interface) return
-
-	    result << method
-    }
-  })
-  result
-}
-
-static List<PsiField> allFieldsIn(PsiJavaFile javaFile) {
-	def result = []
-	javaFile.acceptChildren(new JavaRecursiveElementVisitor() {
-		@Override void visitField(PsiField field) { result << field }
-	})
-	result
-}
-
-static int amountOfParametersIn(PsiMethod method) {
-  method.parameterList.parametersCount
-}
-
 String pluginPath() { pluginPath }
 
 class Histogram {
@@ -54,6 +28,61 @@ class Histogram {
 	}
 }
 
+class ProjectHistograms {
+	def amountOfMethodsInClasses = new Histogram()
+	def amountOfFieldsInClasses = new Histogram()
+	def amountOfParametersInMethods = new Histogram()
+
+	ProjectHistograms process(Iterator<PsiFileSystemItem> items) {
+		for (PsiFileSystemItem item : items) {
+			if (item == null || !(item instanceof PsiJavaFile)) continue
+
+			def methods = allMethodsIn(item)
+			def fields = allFieldsIn(item)
+
+			amountOfFieldsInClasses.add(fields.size())
+			amountOfMethodsInClasses.add(methods.size())
+			methods.each{
+				amountOfParametersInMethods.add(amountOfParametersIn(it))
+			}
+		}
+		this
+	}
+
+	ProjectHistograms persist(String path, String name) {
+		amountOfFieldsInClasses.persist(path, name)
+		amountOfMethodsInClasses.persist(path, name)
+		amountOfParametersInMethods.persist(path, name)
+		this
+	}
+
+	private static List<PsiMethod> allMethodsIn(PsiJavaFile javaFile) {
+		def result = []
+		javaFile.acceptChildren(new JavaRecursiveElementVisitor() {
+			@Override void visitMethod(PsiMethod method) {
+				// ignore interfaces assuming that everything will be counted in their implementations
+				// or if there are not implementations, then it probably can be ignored
+				if (method.containingClass.interface) return
+
+				result << method
+			}
+		})
+		result
+	}
+
+	private static List<PsiField> allFieldsIn(PsiJavaFile javaFile) {
+		def result = []
+		javaFile.acceptChildren(new JavaRecursiveElementVisitor() {
+			@Override void visitField(PsiField field) { result << field }
+		})
+		result
+	}
+
+	private static int amountOfParametersIn(PsiMethod method) {
+		method.parameterList.parametersCount
+	}
+}
+
 
 registerAction("miscProjectHistograms", "ctrl shift H") { AnActionEvent event ->
   def project = event.project
@@ -69,37 +98,23 @@ registerAction("miscProjectHistograms", "ctrl shift H") { AnActionEvent event ->
 def buildHistogramFor(Project project) {
 	doInBackground("Building histograms") {
 		runReadAction {
-			def amountOfMethodsInClasses = new Histogram()
-			def amountOfFieldsInClasses = new Histogram()
-			def amountOfParametersInMethods = new Histogram()
-
-			for (PsiFileSystemItem item : allPsiItemsIn(project)) {
-				if (item == null || !(item instanceof PsiJavaFile)) continue
-
-				def methods = allMethodsIn(item)
-				def fields = allFieldsIn(item)
-
-				amountOfFieldsInClasses.add(fields.size())
-				amountOfMethodsInClasses.add(methods.size())
-				methods.each{
-					amountOfParametersInMethods.add(amountOfParametersIn(it))
-				}
-			}
-
-			amountOfFieldsInClasses.persist(pluginPath(), project.name)
-			amountOfMethodsInClasses.persist(pluginPath(), project.name)
-			amountOfParametersInMethods.persist(pluginPath(), project.name)
-
-			def file = createFromTemplate("${pluginPath}/templates", "histogram.html", project.name, [
-					"project_name_placeholder" : { project.name },
-					"parameters_per_method_data": { asJsArray(amountOfParametersInMethods.map) },
-					"fields_per_class_data": { asJsArray(amountOfFieldsInClasses.map) },
-					"methods_per_class_data": { asJsArray(amountOfMethodsInClasses.map) },
-			])
-
-			BrowserUtil.open("file://${file.absolutePath}")
+			def histograms = new ProjectHistograms()
+					.process(allPsiItemsIn(project))
+					.persist(pluginPath(), project.name)
+			openInBrowser(fillTemplateFrom(histograms))
 		}
 	}
-
 }
+
+File fillTemplateFrom(ProjectHistograms histograms) {
+	createFromTemplate("${pluginPath()}/templates", "histogram.html", project.name, [
+			"project_name_placeholder": { project.name },
+			"parameters_per_method_data": { asJsArray(histograms.amountOfParametersInMethods.map) },
+			"fields_per_class_data": { asJsArray(histograms.amountOfFieldsInClasses.map) },
+			"methods_per_class_data": { asJsArray(histograms.amountOfMethodsInClasses.map) },
+	])
+}
+
+static openInBrowser(File file) { BrowserUtil.open("file://${file.absolutePath}") }
+
 show("reloaded")
